248c749d934bd33f09b1719dd5c2a9ef4ff33f0e
Page Telegram <43476661+pagetelegram@users.noreply.github.com>
2021-06-06 20:13:53 -0500

    Update split.sh

--- split.sh
+++ split.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ffmpeg -i $1 -f mp3 -acodec libmp3lame -filter:a loudnorm -ab 320000 -ar 44100 _$1.mp3
-mp3splt -s -p th=-50,nt=$2 _$1.mp3
+ffmpeg -i $1 -f mp3 -acodec libmp3lame _$1.mp3
+mp3splt -s -p th=-30,nt=$2 _$1.mp3
 #mp3splt -s $1
